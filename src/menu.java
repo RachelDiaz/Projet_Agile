024d38cbe0382efb4d7d9b84f40a35ddfb602914
frederic-prochnow <frederic.prochnow@etudiant.univ-lille1.fr>
2016-08-31 18:19:53 +0200

    MENU

---
+++ src/menu.java
@@ -0,0 +1,20 @@
+
+public class menu {
+
+	public menu() {
+		System.out.println("BIENVENUE AU CASINO ! ");
+		System.out.println("");
+		System.out.println("Choisisez votre jeu");
+		System.out.println("1 BLACKJACK");
+		System.out.println("2 ROULETTE ");
+		System.out.println("3 MACHINE A SOUS");
+		System.out.println("");
+		System.out.println("Bonne Chance");
+	}
+
+	public static void main(String[] args) {
+		new menu();
+
+	}
+
+}
