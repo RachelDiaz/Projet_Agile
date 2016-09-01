befba664b056fc563283f5a13014deed9ca08726
Frederic Prochnow <frederic.prochnow@numericable.fr>
2016-09-01 15:11:37 +0200

    blackjack test

--- src test/blackjack/TestJeuxCartes.java
+++ src test/blackjack/TestJeuxCartes.java
@@ -15,6 +15,7 @@ public class TestJeuxCartes {
 	@Test
 	public void testLength() {
 		JeuxCartes unJeux = new JeuxCartes();
+		unJeux.JeuxCartesClassique ();
 		assertEquals (unJeux.length(), 52);	
 	}
 	
@@ -22,10 +23,13 @@ public class TestJeuxCartes {
 	public void testMelange () {
 		JeuxCartes unJeuxOri = new JeuxCartes ();
 		JeuxCartes unJeux = new JeuxCartes();
+		int nb = 0;
 		unJeux.melanger();
 		
-		//for (int i = 0; i<unJeux.length(); i++)
-		//	if unJeux.
+		for (int i = 0; i<unJeux.length(); i++)
+			if (! unJeux.donner().equals(unJeuxOri.donner())) nb++;
+		
+		assertTrue (nb < 6);
 	}
 
 }
