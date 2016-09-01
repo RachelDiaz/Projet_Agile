fc3083d7550e84f88253c647772b6d0fd8e0df42
Frederic Prochnow <frederic.prochnow@numericable.fr>
2016-09-01 11:32:58 +0200

    blackjack distributio

--- src/blackjack/sabot.java
+++ src/blackjack/sabot.java
@@ -9,7 +9,7 @@ public class sabot {
 	public sabot() {
 		JeuxCartes j1 = new JeuxCartes();
 		for(int j=0; j<4; j++) {
-			//j1.melanger();
+			j1.melanger();
 			for(int i=0; i<j1.length();i++){
 				Deck.add(j1.myJeux.get(i));
 			}
