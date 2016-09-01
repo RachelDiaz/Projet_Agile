0b80a073c2813ae8e8914f364aa24cb7a1cedc22
Rachel Diaz <rachel.diaz@live.fr>
2016-09-01 11:27:17 +0200

    ChoixMise_Main3

---
+++ src/Main/Main.java
@@ -0,0 +1,12 @@
+package Main;
+import Joueur.Joueur;
+
+public class Main {
+	public static Joueur jacky = new Joueur("Jacky");
+	public static Menu menu;
+	
+	public static void main(String[] args) {
+		menu = new Menu();
+	}
+
+}
